f4b0ebd43fd1c2b23be30b970ad0f223bfdd7002
Amir Sandoval <asandovalx9@gmail.com>
2014-11-18 00:20:38 -0800

    test3

--- Project3-Scala/src/Project3.scala
+++ Project3-Scala/src/Project3.scala
@@ -2,7 +2,7 @@ object Project3 {
   def main(args : Array[String]) {
     val list = List(1,2,3,4,5,6,7,8,9,10)
     val updatedList = list map {_ * 3} filter {_ % 2 == 0}
-    println(updatedList)
+    println(updatedList.map{_ + 1})
     println("Hello World!");
   }
 }
